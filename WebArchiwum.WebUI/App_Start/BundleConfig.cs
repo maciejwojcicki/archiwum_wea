d90035f3f9c226b99931997e939552d1aaadd649
maciejwojcicki <mike050991@gmail.com>
2015-05-27 19:37:44 +0200

    push na azure

--- WebArchiwum.WebUI/App_Start/BundleConfig.cs
+++ WebArchiwum.WebUI/App_Start/BundleConfig.cs
@@ -9,10 +9,8 @@ namespace WebArchiwum.WebUI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/underscore.js",
-                        "~/Scripts/underscore-min.js",
-                        "~/Scripts/backbone-min.js"));
+                        "~/Scripts/jquery-{version}.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.js"));
